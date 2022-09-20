60aef1bb15c100c0fbe083adb1b16e0717fcee2d
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-09-20 22:54:25 +0100

    [Feature]: Got involved

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+
+# Installing Nginx web server using puppet
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Ddilibe permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
