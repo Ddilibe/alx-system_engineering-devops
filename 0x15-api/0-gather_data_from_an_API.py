77e59ba9aa4430929dc68ef0250749ff4f6658cf
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-10-17 18:29:59 -0700

    [Fix]: Made some change to an error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,16 +5,18 @@ TODO list progress
 if __name__ == '__main__':
     import requests
     import sys
-    a, b, new_dict = 0, 0, {}
-    name = requests.get('https://jsonplaceholder.typicode.com/users/1/')
+    TOTAL_NUMBER_OF_TASKS, NUMBER_OF_DONE_TASKS, new_dict = 0, 0, {}
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}/'\
+            .format(sys.argv[1]))
     info = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'\
             .format(sys.argv[1]))
-    name = name.json().get("name")
+    EMPLOYEE_NAME = name.json().get("name")
     for i in info.json():
         if i.get("completed") == True:
-            b += 1
-            new_dict[b] = i.get('title')
-        a += 1
-    print("Employee {} is done with tasks({}/{}):".format(name, b, a))
+            NUMBER_OF_DONE_TASKS += 1
+            new_dict[NUMBER_OF_DONE_TASKS] = i.get('title')
+        TOTAL_NUMBER_OF_TASKS += 1
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for i in new_dict.values():
-        print("\t{}".format(i))
+        print("\t {}".format(i))
